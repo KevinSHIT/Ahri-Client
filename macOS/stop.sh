fd66c6491af0a37bc9b6672c957a4555e5bc3896
KevinZonda <203377325@qq.com>
2020-03-07 10:52:12 +0800

    macOS: optimize script & add gitignore

--- macOS/stop.sh
+++ macOS/stop.sh
@@ -6,14 +6,14 @@ echo "========================================"
 
 cd `dirname $0`
 
+echo "- Setting OS X Wi-Fi proxy..."
+echo "  - Authenticate access"
+sudo networksetup -setsocksfirewallproxy Wi-Fi '' '' && networksetup -setsocksfirewallproxystate Wi-Fi off
+
 echo "- Stopping Ahri..."
 bash ./Ahri/stop.sh
 
 echo "- Stopping Clash..."
 bash ./Clash/stop.sh
-
-echo "- Setting OS X Wi-Fi proxy..."
-echo "  - Authenticate access"
-sudo networksetup -setsocksfirewallproxy Wi-Fi '' '' && networksetup -setsocksfirewallproxystate Wi-Fi off
 
 echo "- All things finished, Goodbye~"
